8567eafb442c4040f30dfb9ea01b00b1637df071
Sonal Bhople <sonalbhople777@gmail.com>
2026-08-24 13:49:55 -0700

    folder structure added

---
+++ template.sh
@@ -0,0 +1,16 @@
+## create folder
+mkdir -p src
+mkdir -p research
+
+## create files
+touch research/trials.ipynb
+touch src/__init__.py
+touch src/helper.py
+touch src/prompt.py
+touch .env
+touch requirements.txt
+touch setup.py
+touch app.py
+
+
+echo " directory and files created "
